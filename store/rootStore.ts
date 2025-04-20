fdfce5bbaba45a9c59f2ff8e20d678f9fd067abb
joy <joyadhikary074@gmail.com>
2025-04-20 17:58:45 +0600

    devtools added

--- store/rootStore.ts
+++ store/rootStore.ts
@@ -1,49 +1,19 @@
-// import { create } from "zustand";
-// import { devtools, persist } from "zustand/middleware";
-// import { InstructorState, InstructorStore } from "./instructorStore";
-// import { CourseState, courseStore } from "./courseStore";
-
-// export type RootState = {
-//   instructor: InstructorState;
-//   course: CourseState;
-// };
-
-// const useRootStore = create<RootState>()(
-//   devtools(
-//     persist(
-//       (set, get) => ({
-//         instructor: {
-//           instructors: InstructorStore.instructors,
-//           addInstructor: InstructorStore.addInstructor(set),
-//           removeInstructor: InstructorStore.removeInstructor(set),
-//           getTotalInstructors: InstructorStore.getTotalInstructors(get),
-//         },
-//         course: {
-//           courses: courseStore.courses,
-//           addCourse: courseStore.addCourse(set),
-//           removeCourse: courseStore.removeCourse(set),
-//           updateCourseStatus: courseStore.updateCourseStatus(set),
-//           getAllPrice: courseStore.getAllPrice(get),
-//         },
-//       }),
-//       {
-//         name: "RootStore",
-//         version: 2,
-//       }
-//     )
-//   )
-// );
-
-// export default useRootStore;
-
 import { create } from "zustand";
 import { BearSlice, FishSlice } from "./bear";
 import { createBearSlice } from "./bear";
 import { createFishSlice } from "./fish";
+import { devtools } from "zustand/middleware";
 
-const useBoundStore = create<BearSlice & FishSlice>()((...a) => ({
-  ...createBearSlice(...a),
-  ...createFishSlice(...a),
-}));
+const useBoundStore = create<BearSlice & FishSlice>()(
+  devtools(
+    (...a) => ({
+      ...createBearSlice(...a),
+      ...createFishSlice(...a),
+    }),
+    {
+      name: "bear-fish-store",
+    }
+  )
+);
 
 export default useBoundStore;
